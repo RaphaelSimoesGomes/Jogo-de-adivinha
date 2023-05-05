9de707b77420e5326f7cb32ac201d9817a539425
R. Simões <95584209+simoesxs@users.noreply.github.com>
2023-05-04 21:11:45 -0300

    Add files via upload

---
+++ papyto.py
@@ -0,0 +1,32 @@
+import random
+
+i = 7
+print(f"\nVocê tem {i} tentativas")
+Question = int(input("Qual número você acha que foi sorteado?\n>>> "))
+
+
+valO = random.randint(1,100)
+print(valO)
+while i != 0:
+    if (Question < valO):
+        i-=1
+        print("\nTente um número maior desta vez...\n")
+
+        
+    if (Question > valO):
+        i-=1
+        print("\nTente um número menor desta vez...\n")
+    
+    if Question == valO:
+        print("\nParabénss!!!!!\nVocê descobriu o número perdido")
+        break
+    print(f"\nVocê tem {i} tentativas restantes\n")
+
+    Question = int(input("Qual número você acha que foi sorteado?\n>>> "))
+
+if i == 0:
+        result = int(input(f"\nVocê perdeu gostaria de ver o resultado?\n(1)Sim\n(2)Não\n>>> "))
+        if result ==1:
+            print(f"O número sorteado foi {valO}")
+        if result !=1:
+            print("Ok, tchau")
