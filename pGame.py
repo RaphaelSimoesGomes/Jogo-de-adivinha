48c59439218f4f78c200481e518b8c472de00ac6
R. Simões <95584209+simoesxs@users.noreply.github.com>
2023-05-07 10:33:01 -0300

    Update and rename papyto.py to pGame.py

--- pGame.py
+++ pGame.py
@@ -25,7 +25,7 @@ while i != 0:
     Question = int(input("Qual número você acha que foi sorteado?\n>>> "))
 
 if i == 0:
-        result = int(input(f"\nVocê perdeu gostaria de ver o resultado?\n(1)Sim\n(2)Não\n>>> "))
+        result = int(input(f"\nVocê perdeu gostaria de ver o resultado?\n(1)Sim\n(2)Não\n\n>>> "))
         if result ==1:
             print(f"O número sorteado foi {valO}")
         if result !=1:
